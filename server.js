af53aa62524c24b70acf428cb28d59b1fe568142
itzamary jimenez <itzamary1gonzalez@hotmail.com>
2022-06-30 00:36:40 -0500

    fixed the file path for the routes.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
+app.use(require('./routes/api'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pwa-budget-tracker', {
   useNewUrlParser: true,
